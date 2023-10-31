c528321b25a56c4bf5e242d3a131cdca1b2fb20d
ZetoOfficial <tpptgs@bk.ru>
2023-10-31 09:25:29 +0500

    some upd

--- src/mainUI.py
+++ src/mainUI.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'mainjIYEge.ui'
-##
-## Created by: Qt User Interface Compiler version 6.4.3
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
 from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect)
 from PySide6.QtWidgets import (QGroupBox, QLabel,
                                QPushButton, QRadioButton, QTextEdit)
